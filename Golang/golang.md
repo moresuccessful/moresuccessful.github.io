77bbdba47e2ead10444ac07bda7f8c56f9a789bb
moresuccessful <443700139@qq.com>
2020-02-05 08:57:08 +0800

    2020.02.05

--- Golang/golang.md
+++ Golang/golang.md
@@ -1,6 +1,10 @@
 # Golang
 
+# 数据
+
 ## slice
+
+# 并发
 
 ## 携程
 
@@ -8,9 +12,18 @@
 
 ### 底层实现
 
+# 性能与调试
+
 ## pprof
 
 ## go marshal
 
+# 工程
+
 ## 代码依赖
 vendor gradlew
+
+# 代码规范
+
+## 参考
+1. [github CodeReview](https://github.com/golang/go/wiki/CodeReviewComments)
--- gitbook_commit.sh
+++ gitbook_commit.sh
@@ -1,6 +1,14 @@
-DIR_PATH="/Users/melody/code/moresuccessful.github.io"
-cp -r /Users/melody/know/CommTech/gitbook/_book/* $DIR_PATH
-cd $DIR_PATH
+# upload markdown files
+GITBOOK_PATH="/Users/melody/know/CommTech/gitbook"
+cd $GITBOOK_PATH
+git add .
+git commit -m "$1"
+git push
+
+# upload html files
+HTML_PATH="/Users/melody/code/moresuccessful.github.io"
+cp -r $GITBOOK_PATH/_book/* $HTML_PATH
+cd $HTML_PATH
 git add .
 git commit -m "$1"
 git push

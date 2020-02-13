d1ff528f2e3f13df9481cad6959cd6426160ee9b
moresuccessful <443700139@qq.com>
2020-02-13 23:13:15 +0800

    daily

--- gitbook_commit.sh
+++ gitbook_commit.sh
@@ -7,7 +7,8 @@ git push
 
 # upload html files
 HTML_PATH="/Users/melody/code/moresuccessful.github.io"
-cp -r $GITBOOK_PATH/_book/* $HTML_PATH
+#cp -r $GITBOOK_PATH/_book/* $HTML_PATH
+cp `ls $GITBOOK_PATH/_book |grep -v $GITBOOK_PATH/_book/Self/daily.md |xargs` $HTML_PATH
 cd $HTML_PATH
 git add .
 git commit -m "$1"
